fc93677d8043fd51e9d10376cb7e7ced6751bc8c
mucassah <mukasaaustin3@gmail.com>
2023-10-04 14:12:03 -0700

    javascript web jquery

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,8 @@
+$('document').ready(function () {
+  $('INPUT#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
+      if (e.keyCode === 13) {
+        translate();
+      }
+    });
